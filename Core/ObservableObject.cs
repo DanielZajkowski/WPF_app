15928ddfa6c401d315210dad690ddc555a21f3b2
DanielZajkowski <zajkowskidaniel@gmail.com>
2021-07-26 15:04:09 +0200

    Add project files.

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernDesign.Core
+{
+    // INotifyPropertyChanged - mainly use to updating the ui when binding
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
